45322aeccb7d1bd3f6cec77b0c1d9d2bd4dd8340
MykolaServetnyk <mikokobass@gmail.com>
2024-04-04 15:57:29 +0300

    some-styles

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,8 +1,9 @@
 import css from './Description.module.css';
+import { SiCoffeescript } from "react-icons/si";
 export default function Description() {
    return (
     <div className={css.desc}>
-    <h1>Sip Happens Café</h1>
+           <h1>Sip Happens Café <SiCoffeescript/></h1>
     <p>Please leave your feedback about our service by selecting one of the options below.</p>   
     </div>)
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,12 +1,16 @@
 import css from './Feedback.module.css';
+import { BsEmojiGrin } from "react-icons/bs";
+import { BsEmojiExpressionless } from "react-icons/bs";
+import { BsEmojiAngry } from "react-icons/bs";
+
 
 export default function Feedback({values, totalFeedback, positiveFeedback}) {
     
         return (
     <div>
-        <p className={css.value}>Good: {values.good}</p>
-        <p className={css.value}>Neutral: {values.neutral}</p>
-        <p className={css.value}>Bad: {values.bad}</p>
+        <p className={css.value}><BsEmojiGrin className={css.iconGreen}/> Good: {values.good} </p>
+        <p className={css.value}><BsEmojiExpressionless className={css.iconOrange}/> Neutral: {values.neutral} </p>
+        <p className={css.value}><BsEmojiAngry className={css.iconRed}/> Bad: {values.bad} </p>
         <p className={css.value}>Total: {totalFeedback}</p>
         <p className={css.value}>Positive: { positiveFeedback}</p>
     </div>
