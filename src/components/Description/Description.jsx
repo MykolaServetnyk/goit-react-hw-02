944e463231ded3e0d86d1f70b5c91437cc9091f6
MykolaServetnyk <mikokobass@gmail.com>
2024-04-04 15:17:18 +0300

    first-attempt-final-version

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,7 +1,7 @@
-
+import css from './Description.module.css';
 export default function Description() {
    return (
-    <div>
+    <div className={css.desc}>
     <h1>Sip Happens Café</h1>
     <p>Please leave your feedback about our service by selecting one of the options below.</p>   
     </div>)
